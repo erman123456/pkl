ec2dc091a45bb255caa3c8672f6d979c2b0b61f7
erman123456 <ermansibarani9@gmail.com>
2020-11-18 11:40:58 +0700

    '

---
+++ index.php
@@ -0,0 +1,35 @@
+
+
+               <?php
+
+               $nik = str_replace("'", '', $data[1]);
+					$nip = str_replace("'", '', $data[2]);
+					$nip_lama = str_replace("'", '', $data[3]);
+					$nama = $data[4];
+					$gelar_depan = $data[5];
+					$gelar_belakang = $data[6];
+					$tampat_lahir = $data[7];
+					$kabupaten_lahir = $data[8];
+					$tgl = $data[9];
+					$jenkel = $data[10];
+					$usia = $data[11];
+					$npwp = $data[12];
+					$agama = $data[13];
+					$status_kawin = $data[14];
+					$alamat_jalan = $data[15];
+					$alamat_kelurahan = $data[16];
+					$alamat_kecamatan = $data[17];
+					$alamat_id_kabupaten = $data[18];
+					$alamat_id_provinsi = $data[19];
+					$tinggi_badan = $data[20];
+					$berat_badan = $data[21];
+					$rambut = $data[22];
+					$bentuk_muka = $data[23];
+					$warna_kulit = $data[18];
+					$ciri_khas = $data[18];
+					$cacat_tubuh = $data[18];
+					$kegemaran = $data[18];
+               $tmt_kgb = $data[18];
+
+
+               ?>
