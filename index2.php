a971bcac63f9aba3b3260b9cf5611988fe3764ca
4kramfais <makramfais@gmail.com>
2020-11-18 12:47:18 +0700

    simpan

---
+++ index2.php
@@ -0,0 +1,55 @@
+<?php
+// data lain lain
+$nama = $data[1];
+$pejabat_keterangan = $data[2];
+$nomor_keterangan = $data[3];
+$tanggal_keterangan = $data[4];
+
+// data perguruan
+$nama = $data[1];
+$kedudukan = $data[2];
+$tahun_awal = $data[3];
+$tahun_akhir = $data[4];
+$waktu = $data[5];
+$tempat = $data[6];
+$pimpinan = $data[7];
+$keterangan = $data[8];
+
+//data SLTA
+$nama = $data[1];
+$kedudukan = $data[2];
+$tahun_awal = $data[3];
+$tahun_akhir = $data[4];
+$waktu = $data[5];
+$tempat = $data[6];
+$pimpinan = $data[7];
+$keterangan = $data[8];
+
+//data saudara
+$nama = $data[1];
+$jenkel = $data[2];
+$tanggal_lahir = $data[3];
+$pekerjaan = $data[4];
+$keterangan = $data[5];
+
+//data mertua
+$nama = $data[1];
+$status = $data[2];
+$tanggal_lahir = $data[3];
+$pekerjaan = $data[4];
+$keterangan = $data[5];
+
+//data ortu
+$nama = $data[1];
+$status = $data[2];
+$tanggal_lahir = $data[3];
+$pekerjaan = $data[4];
+$keterangan = $data[5];
+
+//data anak
+$nama = $data[1];
+$jenkel = $data[2];
+$tempat_lahir = $data[3];
+$tanggal_lahir = $data[4];
+$pekerjaan = $data[5];
+$keterangan = $data[6];
